c8a93282322ec034d7498e5abb9163735506945f
Leon Gaban <leongaban@gmail.com>
2018-03-09 09:55:56 -0600

    fixed Board route

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { browserHistory, HashRouter, Route, Switch } from 'react-router-dom';
 import {
-  Portfolio,
+  Board,
   NoMatch
 } from 'containers';
 
 const Routes = () => (
   <HashRouter history={browserHistory}>
     <Switch>
-      <Route exact path="/" component={Portfolio} />
+      <Route exact path="/" component={Board} />
       <Route component={NoMatch} />
     </Switch>
   </HashRouter>
