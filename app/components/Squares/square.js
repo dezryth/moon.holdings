a57ef895060e4e6e3e387402a27883c8ba834231
Leon Gaban <leongaban@gmail.com>
2018-05-11 14:29:06 -0500

    coin styles wired up!

--- app/components/Squares/square.js
+++ app/components/Squares/square.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { floorCents } from 'utils/math';
+import * as style from 'styles/coins';
 
 const calculate = coin => floorCents(coin.balance * coin.price_usd);
+const setStyle = id => style[id];
 
 export default coin => (
-  <li className="coin-square">
+  <li className="coin-square" style={setStyle(coin.id)}>
     <section>
       <h1>{coin.symbol}</h1>
       <p>Price: ${coin.price_usd}</p>
