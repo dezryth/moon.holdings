d5cc8fc7c37682d5d9af9a54ce167810603d123a
Leon Gaban <leongaban@gmail.com>
2018-05-11 14:23:29 -0500

    updated styles

--- app/components/Portfolio/portfolio.js
+++ app/components/Portfolio/portfolio.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 // Components
-// import Square from 'components/Squares/square';
+import Square from 'components/Squares/square';
 
-const CoinSquare = coin => <li className="coin-square">{coin.symbol}</li>;
+// const CoinSquare = coin => (
+//   <li className="coin-square">
+//     <section>
+//       <p>{coin.balance}</p>
+//       <h1>{coin.symbol}</h1>
+//       <p>1 @ ${coin.price_usd}</p>
+//       <em>= ${calculate(coin)}</em>
+//     </section>
+//   </li>
+// );
 
 const Portfolio = ({ coins }) => (
   <div>
     <ul className="portfolio-container">
-      {coins.map(coin => (<CoinSquare key={coin.id} {...coin} />))}
+      {coins.map(coin => (<Square key={coin.id} {...coin} />))}
     </ul>
   </div>
 );
--- app/components/Squares/square.js
+++ app/components/Squares/square.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { floorCents } from 'utils/math';
 
-export default ({ coin }) => (
-  <div className="coin-box">
-    <h1>Coin boxes go here...</h1>
-    {coin}
-  </div>
+const calculate = coin => floorCents(coin.balance * coin.price_usd);
+
+export default coin => (
+  <li className="coin-square">
+    <section>
+      <h1>{coin.symbol}</h1>
+      <p>Price: ${coin.price_usd}</p>
+      <p>Holdings: {coin.balance}</p>
+      <p className="f18"> ${calculate(coin)}</p>
+    </section>
+  </li>
 );
