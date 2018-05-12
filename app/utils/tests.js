8b79f135f483ed6f233564674dc2977d77d8ee75
Leon Gaban <leongaban@gmail.com>
2018-05-12 16:08:39 -0500

    clean up

--- app/utils/tests.js
+++ app/utils/tests.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 import toJson from 'enzyme-to-json';
 
 configure({ adapter: new Adapter() });
