70a07fba568df391d93169dd0dc1ca6593d159d3
Leon Gaban <leongaban@gmail.com>
2018-05-12 16:11:45 -0500

    test added for Portfolio

---
+++ app/components/Portfolio/portfolio.test.js
@@ -0,0 +1,11 @@
+import { testCommonComponentAttrs } from '../../utils/tests';
+
+import Portfolio from './portfolio';
+
+const coins = [];
+
+describe('<Portfolio /> component', () => {
+  testCommonComponentAttrs(Portfolio, {
+    coins
+  });
+});
