f66e0df0ccce84662b8a293c90e2339d38f30f47
Leon Gaban <leongaban@gmail.com>
2018-05-12 12:37:36 -0500

    added Coin styles to SquareEdit

--- app/components/Squares/square.js
+++ app/components/Squares/square.js
@@ -1,12 +1,16 @@
 import React from 'react';
+
+// Utils
 import { floorCents } from 'utils/math';
+import { setStyle } from 'utils/modifiers';
+
+// Styles
 import * as style from 'styles/coins';
 
 const calculate = coin => floorCents(coin.balance * coin.price_usd);
-const setStyle = id => style[id];
 
 export default coin => (
-  <li className="coin-square" style={setStyle(coin.id)}>
+  <li className="coin-square" style={setStyle(coin.id, style)}>
     <section>
       <h1>{coin.symbol}</h1>
       <p>Price: ${coin.price_usd}</p>
