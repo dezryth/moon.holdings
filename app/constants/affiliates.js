3ebafd4310735835b37cf8f7762f2d4351d27b90
Leon Gaban <leongaban@gmail.com>
2018-06-17 13:39:34 -0500

    added affilite links

---
+++ app/constants/affiliates.js
@@ -0,0 +1,3 @@
+export const coinbase = 'https://www.coinbase.com/join/51af71fbe2dfc50f3d000024';
+export const binance = 'https://www.binance.com/?ref=10066165';
+export const changelly = 'https://changelly.com/?ref_id=167324d59a32';
